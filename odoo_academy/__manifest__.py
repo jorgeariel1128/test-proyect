632fc2a38286209427ec2c7e8613a2c765e94386
jorgeariel1128 <jorge.hernandez@nubosoft.com>
2022-09-20 03:02:57 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,24 @@
+#-*- coding: utf-8 -*-
+{
+    'name': 'Odoo Academy',
+    'Summary': """Academy app to manage Training""",
+    'description': """
+        Academy Module to manege Training:
+        - Courses
+        - Sessions
+        - Atendees
+    
+    """,
+    'authot': 'Jorge',
+    'website': 'https://slyn.io',
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data':[],
+    'demo': [],
+    
+    
+    
+}
